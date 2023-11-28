aadb72e6f60f01f4ea229aca480f88aa71e43dbe
Anthony OConnell <tony@one.ie>
2023-11-28 14:15:49 +0700

    Letsgo

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "ONE",
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+            { label: "Example test", link: "/guides/test/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
---
+++ src/content/docs/guides/test.md
@@ -0,0 +1,15 @@
+---
+title: Page with a banner
+banner:
+  content: |
+    We just launched something cool!
+    <a href="https://example.com">Check it out</a>
+---
+
+This page describes how to use Markdown in Starlight.
+:::tip[Did you know?]
+Astro helps you build faster websites with [“Islands Architecture”](https://docs.astro.build/en/concepts/islands/).
+:::
+## Inline Styles
+
+## Headings
